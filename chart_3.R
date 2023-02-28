10ea47cad1b8c97b8cbb9b2cd4e9678e4673d690
tr206-2069586 <92127169+tr206-2069586@users.noreply.github.com>
2023-02-28 15:24:36 -0800

    Delete chart_3.R

--- chart_3.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-ac_data <- read.csv("Accident Dataset (Cleaned).csv")
-ggplot(data = ac_data) + geom_boxplot(aes(x = Weather, y = EMS_Arrival_Time)) + 
-  labs(y = 'EMS Arrival Time (hours)', title = 'Boxplot of EMS Arrival Time in Different Weather') +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
