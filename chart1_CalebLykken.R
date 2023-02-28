4e84a21f6839699501a368da7981078b4bd24e44
tr206-2069586 <92127169+tr206-2069586@users.noreply.github.com>
2023-02-28 15:19:15 -0800

    Delete chart1_CalebLykken.R

--- chart1_CalebLykken.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(plotly)
-ac_data <- read.csv("accident.csv")
-
-deaths_persons_correlation <- ggplot(ac_data, aes(PERSONS, FATALS)) + geom_point()
-
-ggplotly(deaths_persons_correlation)
