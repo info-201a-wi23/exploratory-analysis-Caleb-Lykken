98d3fa9597a66d5a4f79a0c08e7e417eb2c8bb76
Caleb-Lykken <121900001+Caleb-Lykken@users.noreply.github.com>
2023-02-28 15:19:13 -0800

    Delete chart1_QingDeng.R

--- chart1_QingDeng.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-library(plotly)
-accident_data <- read.csv("Accident Dataset (Cleaned).csv")
-
-deaths_people_weather <- ggplot(data = accident_data) + 
-  geom_col(mapping = aes(x = Weather, y = FATALS)) 
-
-ggplotly(deaths_people_weather)
